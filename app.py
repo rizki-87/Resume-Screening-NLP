e20c1f2b36975047211d09415287c9b79dde39b5
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-01-28 18:46:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,36 @@ with open('tfidfd.pkl', 'rb') as f:
 with open('clf.pkl', 'rb') as f:
     classification_model = pickle.load(f)
 
+category_mapping = {
+    0: "Advocate",
+    1: "Arts",
+    2: "Automation Testing",
+    3: "Blockchain",
+    4: "Business Analyst",
+    5: "Civil Engineer",
+    6: "Data Science",
+    7: "Database",
+    8: "DevOps Engineer",
+    9: "DotNet Developer",
+    10: "ETL Developer",
+    11: "Electrical Engineering",
+    12: "HR",
+    13: "Hadoop",
+    14: "Health and Fitness",
+    15: "Java Developer",
+    16: "Mechanical Engineer",
+    17: "Network Security Engineer",
+    18: "Operations Manager",
+    19: "PMO",
+    20: "Python Developer",
+    21: "SAP Developer",
+    22: "Sales",
+    23: "Testing",
+    24: "Web Designing",
+    # Jika kategori prediksi tidak ada dalam kamus ini, akan dikembalikan "Unknown"
+    'outside the data above': "Unknown"
+}
+
 st.title("Resume Screening with NLP")
 
 uploaded_file = st.file_uploader("Upload your resume in PDF format:", type="pdf")
@@ -45,5 +75,8 @@ if uploaded_file is not None:
     vectorized_text = tfidf_vectorizer.transform([cleaned_text])
     prediction = classification_model.predict(vectorized_text)
     
-    st.write(f"Predicted Category: {prediction[0]}")
+    # Mendapatkan kategori berdasarkan prediksi
+    predicted_category = category_mapping.get(prediction[0], "Unknown")
+    
+    st.write(f"Predicted Category: {predicted_category}")  # Menampilkan kategori bukan angka
     # Tambahkan lebih banyak feedback instan dan analisis di sini
